a442da4e1759421f44f742a4a4cf46845a8812f2
Thomas Duncan <dunc0178@algonquinlive.com>
2025-11-09 21:52:14 -0500

    Finished Async Exercise

--- js/main.js
+++ js/main.js
@@ -5,60 +5,73 @@ async function init() {
     document.querySelector('body').addEventListener('click', clickEvent);
 };
 
-let hex = 0;
+//let h = 0;
 
 function clickEvent(ev) {
     const num = Math.random(0,1);
     console.log(num);
 
-    // return new Promise(function (zero, one).setTimeout(Math.random(1000,2000)) {
-    //     if (Math.round(num) == 0){
-    //         console.log(`Winner: Zero`);
-    //         zero();
-    //     }
-    //     else {
-    //         console.log(`Winner: One`);
-    //         one();
-    //     }
-    // });
-
     if (Math.round(num) == 0){
-        // console.log(`Winner: Zero`);
-        // zero();
-        return new Promise(async (resolve) => {
-            setTimeout((resolve) => {
-                f1();
-                console.log(`Hex: ${hex}`);
-                return resolve = hex;
-            }, Math.random(1000,2000));
-        })
-        .then(resolve => {
-            console.log('Change background colour here'); 
-        });
+
+        console.log(`Winner: Zero`);
+
+        zero();
+        
     }
     else {
-        // console.log(`Winner: One`);
-        // one();
-        return new Promise(async (resolve) => {
-            setTimeout((resolve) => {
-                f2();
-            }, Math.random(1000,2000));
-        });
-    }
-    
 
-    // Promise.any(function (resolve))
-    //     setTimeout(resolve, Math.floor(Math.random(1000,2000)));
+        console.log(`Winner: One`);
+
+        one();
+
+    }
+
+}
+
+function zero(){
+    //Change Background colour
+    let hex = '';
+    return new Promise(f1 => {
+        setTimeout(() => {
+            console.log(`Getting colour...`);
+            f1();
+        }, Math.floor(Math.random() * (2000 - 1000)) + 1000 + 1);
+    })
+    .then((color) => {
+        let h = f1();
+        console.log(h);
+        document.querySelector('body').style.backgroundColor = h;
+    });
+}
+
+function one(){
+    //Add new paragraph
+    return new Promise(f2 => {
+        setTimeout(() => {
+            console.log(`Getting string...`);
+            f2();
+        }, Math.floor(Math.random() * (2000 - 1000)) + 1000 + 1);
+    })
+    .then((str) => {
+        // let h = f1();
+        let p = `<p>${f2()}</p>`;
+        // console.log(h);
+        console.log(p);
+        // document.querySelector('body').style.backgroundColor = h;
+        document.querySelector('main').innerHTML += p;
+    });
 }
 
 function f1() {
-    console.log(`Winner: Zero`);
-    document.querySelector('body').style.backgroundColor ;
-    hex = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-    console.log(hex);
+    let hex = Math.random().toString(16); //generate a random decimal and covert to hexadecimal as a string
+    let extracted = hex.substring(2, 8); //skip the first characters "0." and return 6 characters
+    let color = `#${extracted}`; //add the hashtag to make it a color for css.
+    console.log(color);
+    return color;
 }
 
 function f2() {
-    console.log(`Winner: One`);
-    const p = 'This is a paragraph.';
+    // const p = 'This is a paragraph.';
+    let str = 'This is a paragraph.' 
+    return str;
 }
